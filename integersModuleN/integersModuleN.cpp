dd497fc30bb57a9bc481d2e1caaeea4830bc2cfe
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2019-01-29 20:54:35 -0600

    integersModuleN READYYY

--- integersModuleN/integersModuleN.cpp
+++ integersModuleN/integersModuleN.cpp
@@ -1,63 +1,83 @@
 #include<iostream>
 using namespace std;
 
-void printMatrix(int **m, int t, int d);
-void fillMatrix(int **m, int t, int d);
-
+void fillMatrix(int **m, int t);
+void printMatrix(int **m, int t);
+void fillTriangle(int **m, int cof[], int t, int n);
+void fillTable(int **m, int cof[], int t, int n);
 
 int **matrix;
-
 int main(int argc, char const *argv[]) {
-	int i, j, ti = 12, n = 21;
-	int down = 6;
+	int n, a, b, i, j = 0;
+	int ti = 0; // Columns size
+	cin >> n;
+	int cofactors[n];
+	for(i = 0; i < n; i++) {
+		if(__gcd(i, n) == 1) {
+			cofactors[j] = i;
+			j++;
+			ti++;
+			//cout << i << endl;
+		}
+	}
 
 	matrix = (int**)calloc(ti, sizeof(int*));
 	for (i = 0; i < ti; i++)
-		matrix[i] = (int*)calloc(down, sizeof(double));
+		matrix[i] = (int*)calloc(ti, sizeof(int));
+
+	fillMatrix(matrix, ti);
+	//fillTriangle(matrix, cofactors, ti, n);
+	fillTable(matrix, cofactors, ti, n);
+
+	cout << endl;
 	
-	int cofactors[12] = {1, 2, 4, 5, 8, 10, 11, 13, 16, 17, 19, 20};
+	printMatrix(matrix, ti);
 
-	fillMatrix(matrix, ti, down);
-
-	/*for(i = 0; i < column; i++) 
-		matrix[0][i] = cofactors[i];
-
-	for(i = 0; i < column; i++) {
-		for(j = 0; j < row; j++) {
-			if((i == j) || (j > i)) {
-				matrix[i][j] = ((i - 1) * j) % n;
-
-			}
-			n = 2;
-		}
-	}*/
-	printMatrix(matrix, ti, down);
-/*
-	for(i = 0; i < 12; i++) {
-		for(j = 0; j <12; j++) {
-			//cout << "[" << i << "," << j << "] ";
-		}
-		cout << endl;
-	}
-*/
 	return 0;
 }
 
-void printMatrix(int **m, int t, int d) {
+void fillMatrix(int **m, int t) {
+	int i, j;
+	for(i = 0; i < (t / 2); i++) 
+		for(j = 0; j < t; j++) 
+			matrix[i][j] = -1;
+}
+void printMatrix(int **m, int t) {
 	int i, j;
 	for(i = 0; i < t; i++) {
-		for(j = 0; j < d; j++)
-			cout << "[" << m[i][j] <<"] ";
-
+		for(j = 0; j < t; j++) 
+			cout << "[" << matrix[i][j] << "]";
 		cout << endl;
 	}
 }
 
-// Fill matrix with -1
-void fillMatrix(int **m, int t, int d) {
+void fillTriangle(int **m, int cof[], int t, int n) {
 	int i, j;
-	for(i = 0; i < d; i++) 
-		for(j = 0; j < ti; j++)
-			m[i][j] = -1;
+	int aux = 1;
+	int lim = 0;
+	int f = 0;
+	for(j = 0; j < t; j++) {
+		m[0][j] = cof[j]; 
+	} 
+	for(i = 1; i < (t / 2); i++) {
+		lim = aux;
+		for(j = aux; j < (t - lim); j++) {
+			//cout << "Multiplico:" << cof[j] << " , " << cof[aux] << endl;
+			//m[i][j] = 0;
+			m[i][j] = (cof[j] * cof[aux]) % n;
+		}
+		aux++;
+	}
 }
 
+void fillTable(int **m, int cof[], int t, int n) {
+	int i, j;
+	int aux = 0;
+
+	for(i = 0; i < t; i++) {
+		for(j = 0; j < t; j++) {
+			m[i][j] = (cof[j] * cof[aux]) % n;
+		}
+		aux++;
+	}
+}
