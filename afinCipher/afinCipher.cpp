56a6bae9626181d20691fd727a81b92e4928a0ee
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2019-01-30 23:49:55 -0600

    Intentando...

---
+++ afinCipher/afinCipher.cpp
@@ -0,0 +1,105 @@
+/*
+	Description: We can cipher and decipher
+
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int getNumber(char letter);
+void cipher(int n);
+void decipher(int n);
+//void decipher(int n, int k, string cipher);
+
+int main(int argc, char const *argv[]) {
+    int n = 26, opc = 5;
+    //int i;
+   /* while(opc != 0) {
+	    cout << endl << "\n1 - Cipher" << endl << "2 - Decipher" << endl;
+	    cin >> opc; 
+	    if(opc == 1) 
+		    cipher(n);
+        if(opc == 2)
+		    decipher(n);
+	}
+	*/
+	cipher(27);
+	/*
+	string dec;
+	cin >> dec;
+	for(i = 0; i < 26; i++) {
+		decipher(n, i, dec);
+		cout << endl << endl;
+	}*/
+	return 0;
+}
+
+void cipher(int n) {
+	int i, a, b, k, numLetter, numCip[27];
+	string message;
+	char character = 'A';
+
+	cout << "\n -------------------------------\n";
+	cout << "Write the message | Write a | Write b \n";
+	cin >> message >> a >> b;
+
+	for(i = 0; i < 27; i++) {
+		numCip[i] = -1;
+	}
+	// For each character
+	for(i = 0; i < message.length(); i++) {
+		numLetter = getNumber(message[i]);
+		if(numLetter == -156) {
+			numLetter = 14;
+		}
+		else if(numLetter > 13) {
+			numLetter++;
+		}
+		//cout << numLetter << endl;
+		if(numCip[numLetter] == -1) {
+			numCip[numLetter] = (((2 * numLetter) + b) % n);	
+ 
+		}
+		if(numCip[numLetter] == 14) {
+			character = character + 100;
+		}
+	//	character = character + numCip[numLetter];
+		cout << character;
+	//	character = 'A'; 
+	}
+}
+
+//void decipher(int n, int k, string cipher) {
+void decipher(int n) {
+	int i, k, numLetter, numDec[26];
+	string cipher;
+	char character = 'A';
+
+	cout << "\n -------------------------------\n";
+	cout << "Write the cipher | Write the key \n";
+	cin >> cipher >> k;
+
+	for(i = 0; i < 26; i++) {
+		numDec[i] = -1;
+	}
+	// For each character
+	for(i = 0; i <  cipher.length(); i++) {
+		numLetter = getNumber(cipher[i]);
+		if(numDec[numLetter] == -1) {
+			if((numLetter - k) < 0) {
+				numDec[numLetter] = (numLetter - k + n) % n;
+			}
+			else
+				numDec[numLetter] = (numLetter - k) % n; 
+		}
+		character = character + numDec[numLetter];
+		cout << character;
+		character = 'A';
+	}
+}
+
+int getNumber(char letter) {
+	int i = letter - 65;
+	return i;
+}
