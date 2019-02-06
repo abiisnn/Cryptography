78132d02624bc02bb3672437c5c976fc41e3b1a2
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2019-02-05 21:39:36 -0600

    afinCipher ready

--- afinCipher/afinCipher.cpp
+++ afinCipher/afinCipher.cpp
@@ -8,8 +8,15 @@
 using namespace std;
 
 int getNumber(char letter);
+int getInv(int num, int n);
+void fillMatrix(int **m, int t);
+void printMatrix(int **m, int t);
+void fillTriangle(int **m, int cof[], int t, int n);
+void fillTable(int **m, int cof[], int t, int n);
 void afinCipher(int n);
 void afinDecipher(int n);
+
+int **matrix;
 
 int main(int argc, char const *argv[]) {
     int n = 26, opc = 5;
@@ -58,7 +65,7 @@ void afinCipher(int n) {
 
 //void decipher(int n, int k, string cipher) {
 void afinDecipher(int n) {
-	int i, a, b, numLetter, numDec[26];
+	int i, a, b, numLetter, numDec[n], inverso;
 	string cipher;
 	char character = 'A';
 
@@ -66,7 +73,7 @@ void afinDecipher(int n) {
 	cout << "Write the message | Write a | Write b \n";
 	cin >> cipher >> a >> b;
 
-	for(i = 0; i < 26; i++) {
+	for(i = 0; i < n; i++) {
 		numDec[i] = -1;
 	}
 	// For each character
@@ -74,11 +81,12 @@ void afinDecipher(int n) {
 		numLetter = getNumber(cipher[i]);
 		if(numDec[numLetter] == -1) {
 			numDec[numLetter] = numLetter - b;
+			inverso = getInv(a, n);
 			if(numDec[numLetter] < 0) {
-				numDec[numLetter] = (n + (numDec[numLetter] / a)) % n;
+				numDec[numLetter] = (n + (numDec[numLetter] * inverso)) % n;
 			}
 			else
-				numDec[numLetter] = (numDec[numLetter] / a) % n; 
+				numDec[numLetter] = (numDec[numLetter] * inverso) % n; 
 		}
 		character = character + numDec[numLetter];
 		cout << character;
@@ -89,4 +97,64 @@ void afinDecipher(int n) {
 int getNumber(char letter) {
 	int i = letter - 65;
 	return i;
+}
+void fillMatrix(int **m, int t) {
+	int i, j;
+	for(i = 0; i < (t / 2); i++) 
+		for(j = 0; j < t; j++) 
+			matrix[i][j] = -1;
+}
+void printMatrix(int **m, int t) {
+	int i, j;
+	for(i = 0; i < t; i++) {
+		for(j = 0; j < t; j++) {
+			if( matrix[i][j] < 10) {
+				cout << "0";
+			}
+			cout << matrix[i][j] << " ";	
+		}
+		cout << endl;
+	}
+}
+void fillTable(int **m, int cof[], int t, int n) {
+	int i, j;
+	int aux = 0;
+
+	for(i = 0; i < t; i++) {
+		for(j = 0; j < t; j++) {
+			m[i][j] = (cof[j] * cof[aux]) % n;
+		}
+		aux++;
+	}
+}
+
+int getInv(int num, int n) {
+	int a, b, aux, i, j = 0;
+	int ti = 0; // Columns size
+
+	int cofactors[n];
+	for(i = 0; i < n; i++) {
+		if(__gcd(i, n) == 1) {
+			cofactors[j] = i;
+			j++;
+			ti++;
+			//cout << i << endl;
+		}
+	}
+	matrix = (int**)calloc(ti, sizeof(int*));
+	for (i = 0; i < ti; i++)
+		matrix[i] = (int*)calloc(ti, sizeof(int));
+
+	fillMatrix(matrix, ti);
+	fillTable(matrix, cofactors, ti, n);
+
+	for(i = 0; i < ti; i++) {
+		if(matrix[0][i] == num) {
+			for(j = 0; j < ti; j++) {
+				aux = matrix[j][i];
+				if(aux == 1) 
+					return matrix[j][0];
+			}
+		}
+	}
 }
