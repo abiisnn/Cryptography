1ae50fcad7892383e3d666f46ee883fb56c24012
Me0089mo <31458680+Me0089mo@users.noreply.github.com>
2019-02-11 20:00:37 -0600

    Add files via upload

---
+++ affineCipherComplete/keyWordCipher.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+//NOTA: Caracteres imprimibles de 32-126
+	
+void menu();
+string cipher(string key, string msj);
+string descipher(string key, string msj);
+
+int main(){
+	string key = " ";
+	string msj = " ";
+	int op = 0;
+	do{
+		//Despliegue del menu de opciones
+		menu();
+		cin>>op;
+		//Cifrado del mensaje
+		if(op == 1){
+			cout<<"Ingrese la palabra clave: ";
+			getchar();
+			getline(cin, key);
+			cout<<"Ingrese el mensaje a cifrar: ";
+			getline(cin, msj);
+			cout<<"Mensaje cifrado: " + cipher(key, msj) + "\n";
+		}
+		//Descifrado del mensaje
+		else if(op == 2){
+			cout<<"Ingrese la palabra clave: ";
+			getchar();
+			getline(cin, key);
+			cout<<"Ingrese el mensaje a descifrar: ";
+			getline(cin, msj);
+			cout<<"Mensaje descifrado: " + descipher(key, msj) + "\n";
+		}
+	}while(op != 3);
+	return 0;
+}
+
+void menu(){
+	cout<<"Escoja la opcion que desee realizar: ";
+	cout<<"\n1.- Cifrar";
+	cout<<"\n2.- Descifrar";
+	cout<<"\n3.- Salir         ";
+}
+
+string cipher(string key, string msj){
+	//Obtención del mensaje caracter a caracter
+	int cont3, nuevo_val = 0, aux = 0, n = 95, tam_key = 0, val_c = 0;
+	string msj_cip;
+	char aux_msj;
+	//Tamaño de la palabra clave
+	tam_key = key.size();
+	for(cont3 = 0; cont3 < msj.size(); cont3++){
+		val_c = (int)msj[cont3] - 32;
+		aux = (int)key[cont3 % tam_key] - 32;
+		//Se obtiene el nuevo valor para la letra
+		nuevo_val = (val_c + aux) % n;
+		nuevo_val += 32;
+		aux_msj = (char)nuevo_val;
+		msj_cip += aux_msj;
+	}
+	return msj_cip;
+}
+
+string descipher(string key, string msj){
+	//Obtención del mensaje caracter a caracter
+	int cont3, nuevo_val = 0, aux = 0, n = 95, tam_key = 0, val_c = 0;
+	string msj_descip;
+	char aux_msj;
+	//Tamaño de la palabra clave
+	tam_key = key.size();
+	for(cont3 = 0; cont3 < msj.size(); cont3++){
+		val_c = (int)msj[cont3] - 32;
+		aux = (int)key[cont3 % tam_key] - 32;
+		//Se obtiene el nuevo valor para la letra
+		nuevo_val = val_c - aux;
+		if(nuevo_val < 0)
+			nuevo_val = n + (nuevo_val % n);
+		else
+			nuevo_val = nuevo_val % n;
+		nuevo_val += 32;
+		aux_msj = (char)nuevo_val;
+		msj_descip += aux_msj;
+	}
+	return msj_descip;
+}
