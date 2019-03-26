17a390a33a7215063f8b4d9c0cd66b7279b7a7c8
Abii Snn <anicolass1300@alumno.ipn.mx>
2019-03-26 09:55:24 -0600

    Exercise 3

---
+++ Session6/exe3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int elementSB() {
+	vector<vector<int>> matrix = {
+		{1, 0, 0, 0, 1, 1, 1, 1},
+		{1, 1, 0, 0, 0, 1, 1 ,1},
+		{1, 1, 1, 0, 0, 0, 1, 1},
+		{1, 1, 1, 1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 0, 0, 0},
+		{0, 1, 1, 1, 1, 1, 0, 0},
+		{0, 0, 1, 1, 1, 1, 1, 0},
+		{0, 0, 0, 1, 1, 1, 1, 1}
+	};
+	vector<int> column = {1, 1, 0, 0, 0, 1, 1, 0};
+	
+		
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
+
